fdb8123e1de4913aaa514b61bab6bb4e8eb96887
qazmkp123@gmail.com <qazmkp123@gmail.com>
2017-12-14 21:23:43 +0900

    숫자 표시및 6개초과 선택 방지

--- app/src/main/java/com/example/bbc/own/ResultActivity.java
+++ app/src/main/java/com/example/bbc/own/ResultActivity.java
@@ -10,19 +10,42 @@ import android.widget.TextView;
 public class ResultActivity extends AppCompatActivity {
     int num = MainActivity.score;
     ImageView imageView =null;
-    TextView good;
-    TextView bad;
+    TextView txGood;
+    TextView txGood1;
+    TextView txGood2;
+    TextView txGood3;
+    TextView txBad;
+    TextView txBad1;
+    TextView txBad2;
+    TextView txBad3;
+    int g1 = MainActivity.good[0];
+    int g2 = MainActivity.good[1];
+    int g3 = MainActivity.good[2];
+    int b1 = MainActivity.bad[0];
+    int b2 = MainActivity.bad[1];
+    int b3 = MainActivity.bad[2];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        good =findViewById(R.id.Good);
-        bad = findViewById(R.id.Bad);
-
+        txGood =findViewById(R.id.Good);
+        txGood1 =findViewById(R.id.Good1);
+        txGood2 =findViewById(R.id.Good2);
+        txGood3 =findViewById(R.id.Good3);
+        txBad = findViewById(R.id.Bad);
+        txBad1 = findViewById(R.id.Bad1);
+        txBad2 = findViewById(R.id.Bad2);
+        txBad3 = findViewById(R.id.Bad3);
         imageView = (ImageView)findViewById(R.id.unse);
 
-        
+        txGood1.setText(String.valueOf(g1));
+        txGood2.setText(String.valueOf(g2));
+        txGood3.setText(String.valueOf(g3));
+        txBad1.setText(String.valueOf(b1));
+        txBad2.setText(String.valueOf(b2));
+        txBad3.setText(String.valueOf(b3));
 
         switch (num){
             case 0 : imageView.setImageResource(R.drawable.i1);break;
